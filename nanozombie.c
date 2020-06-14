341c6c41556e0f34596af70aa1ec1457c94d900a
pietrzakkuba <pietrzakkuba@yahoo.pl>
2020-06-14 15:13:26 +0200

    add vectors

--- nanozombie.c
+++ nanozombie.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+#include "vector.h"
+
 #define ROOT 0
 
 //mozliwe ze to niepotrzebne
@@ -41,19 +43,21 @@ void printArray(int *rank, int array[], int *count, char title[])
 }
 
 // rzeczy lamporta
-int max(int a, int b) {
+int max(int a, int b)
+{
     return a > b ? a : b;
 }
 
-
 MPI_Datatype mpiLamportPacket;
 
-typedef struct lamportPacket_s {
+typedef struct lamportPacket_s
+{
     int clock;
     char message[MESSAGE_SIZE];
 } lamportPacket;
 
-int lamportSend(int clock, int src, int dest, char *messageOut) {
+int lamportSend(int clock, int src, int dest, char *messageOut)
+{
     lamportPacket packetOut;
     packetOut.clock = clock + 1;
     strcpy(packetOut.message, messageOut);
@@ -61,7 +65,8 @@ int lamportSend(int clock, int src, int dest, char *messageOut) {
     return packetOut.clock;
 }
 
-lamportPacket lamportReceive(int clock, int src, int dest) {
+lamportPacket lamportReceive(int clock, int src, int dest)
+{
     MPI_Status status;
     lamportPacket packetIn;
     MPI_Recv(&packetIn, 1, mpiLamportPacket, src, MESSAGE_TAG, MPI_COMM_WORLD, &status);
@@ -76,15 +81,14 @@ int main(int argc, char **argv)
 {
     MPI_Init(&argc, &argv);
 
-    // konfiguracja structa dla MPI 
+    // konfiguracja structa dla MPI
     MPI_Datatype types[2] = {MPI_INT, MPI_CHAR};
-    int blocklengths[2] =   {1, MESSAGE_SIZE};
+    int blocklengths[2] = {1, MESSAGE_SIZE};
     MPI_Aint offsets[2];
     offsets[0] = offsetof(lamportPacket, clock);
     offsets[1] = offsetof(lamportPacket, message);
     MPI_Type_create_struct(2, blocklengths, offsets, types, &mpiLamportPacket);
     MPI_Type_commit(&mpiLamportPacket);
-
 
     int size, rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
@@ -103,7 +107,8 @@ int main(int argc, char **argv)
         submarineRangeFrom = 3;
         submarineRangeTo = 10;
     }
-    else {
+    else
+    {
         ponyCostumes = atoi(argv[1]);
         submarineCount = atoi(argv[2]);
         touristRangeFrom = atoi(argv[3]);
@@ -124,9 +129,9 @@ int main(int argc, char **argv)
         //to wtedy po tym ifie wszystko wspolne dla procesow lacznie z nim
         //było coś o tym wysyłaniu sam do siebie w opisie zegaru lamporta chyba
 
-        // printf("tourists: %d\nponyCostumes: %d\nsubmarines: %d\n", touristCount, ponyCostumes, submarineCount);
-        // printf("tourist range: %d-%d\n", touristRangeFrom, touristRangeTo);
-        // printf("submarine range: %d-%d\n", submarineRangeFrom, submarineRangeTo);
+        printf("tourists: %d\nponyCostumes: %d\nsubmarines: %d\n", touristCount, ponyCostumes, submarineCount);
+        printf("tourist range: %d-%d\n", touristRangeFrom, touristRangeTo);
+        printf("submarine range: %d-%d\n", submarineRangeFrom, submarineRangeTo);
 
         //inicjalizacja turystów
         for (int i = 0; i < touristCount; i++)
@@ -140,7 +145,6 @@ int main(int argc, char **argv)
             submarines[i] = getRandom(submarineRangeFrom, submarineRangeTo);
         }
 
-        
         //wysłanie danych do wszystkich procesów
         for (int i = 0; i < size; i++)
         {
@@ -153,27 +157,39 @@ int main(int argc, char **argv)
     MPI_Recv(tourists, touristCount, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
     MPI_Recv(submarines, submarineCount, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
 
-    // printArray(&rank, tourists, &touristCount, "turysci");
-    // printArray(&rank, submarines, &submarineCount, "lodzie");
+    printArray(&rank, tourists, &touristCount, "turysci");
+    printArray(&rank, submarines, &submarineCount, "lodzie");
 
     //tu cala robota procesow
 
     // testowanie komunikacja lamporta - root wysyła do (siebie + 1) i +1 odbiera i printuje msg i clock
-    if (rank == ROOT) {
-        char* msg = "ACK";
-        int clock = 0;
-        clock = lamportSend(clock, ROOT, ROOT + 1, msg);
-    }
-    if (rank == ROOT + 1) {
-        int clock = 0;
-        lamportPacket receivePacket;
-        receivePacket = lamportReceive(clock, ROOT, ROOT + 1);
-        printf("zegar odbiorcy: %d\n", receivePacket.clock);
-        printf("wiadomośc od nadawcy: %s\n", receivePacket.message);
-    }
+    // teraz zakomentowane - możesz sobie przetestować jak chcesz
+    // if (rank == ROOT)
+    // {
+    //     char *msg = "ACK";
+    //     int clock = 0;
+    //     clock = lamportSend(clock, ROOT, ROOT + 1, msg);
+    // }
+    // if (rank == ROOT + 1)
+    // {
+    //     int clock = 0;
+    //     lamportPacket receivePacket;
+    //     receivePacket = lamportReceive(clock, ROOT, ROOT + 1);
+    //     printf("zegar odbiorcy: %d\n", receivePacket.clock);
+    //     printf("wiadomośc od nadawcy: %s\n", receivePacket.message);
+    // }
 
-
-
+    // deklaracja zmiennych lokalnych procesu
+    struct vector listKucykOk;
+    struct vector listKucykHalt;
+    struct vector listLodz;
+    init_vector(&listKucykOk);
+    init_vector(&listKucykHalt);
+    init_vector(&listLodz);
+    
     MPI_Finalize();
+
+
+    
     return 0;
 }
