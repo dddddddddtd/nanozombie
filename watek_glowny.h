34f704f66555dede6cbc167f61bb2e9c5afd46b6
dddddddddtd <kozjach@gmail.com>
2020-06-20 14:56:12 +0200

    clean up

--- watek_glowny.h
+++ watek_glowny.h
@@ -40,68 +40,66 @@ void mainLoop()
         {
         }
 
-        if (stan == LodzQ)
-        {
-            if (nadzorca == -1)
-            {
-                pthread_mutex_lock(&lodzMut);
-                std::sort(LISTlodz.begin(), LISTlodz.end());
-                int index = std::distance(LISTlodz.begin(), std::find(LISTlodz.begin(), LISTlodz.end(), rank));
-                pthread_mutex_unlock(&lodzMut);
+        // if (stan == LodzQ)
+        // {
+        //     if (nadzorca == -1)
+        //     {
+        //         pthread_mutex_lock(&lodzMut);
+        //         std::sort(LISTlodz.begin(), LISTlodz.end());
+        //         int index = std::distance(LISTlodz.begin(), std::find(LISTlodz.begin(), LISTlodz.end(), rank));
+        //         pthread_mutex_unlock(&lodzMut);
 
-                if (index == 0) // jeżeli proces znajduje się na pierwszym miejscu kolejki żądań o łódź
-                {
+        //         if (index == 0) // jeżeli proces znajduje się na pierwszym miejscu kolejki żądań o łódź
+        //         {
 
-                    if (lodzieStan[wybieranaLodz] != 0) // jeżeli wybierana lodz jest dostepna
-                    {
+        //             if (lodzieStan[wybieranaLodz] != 0) // jeżeli wybierana lodz jest dostepna
+        //             {
 
-                        if (LISTlodz.size() + turysciWycieczka >= ponyCostumes) // sprawdzenie, czy wszystkie kostiumy kucyka są zajęte
-                        {
-                            // debug("NADZORUJE TEST (%d): %s", turysciWycieczka, stringLIST(LISTlodz).c_str());
-                            wycieczka.clear();
-                            int suma = 0;
-                            for (int i = 0; i < LISTlodz.size(); i++)
-                            {
-                                suma += tourists[LISTlodz[i].processid];
+        //                 if (LISTlodz.size() + turysciWycieczka >= ponyCostumes) // sprawdzenie, czy wszystkie kostiumy kucyka są zajęte
+        //                 {
+        //                     // debug("NADZORUJE TEST (%d): %s", turysciWycieczka, stringLIST(LISTlodz).c_str());
+        //                     wycieczka.clear();
+        //                     int suma = 0;
+        //                     for (int i = 0; i < LISTlodz.size(); i++)
+        //                     {
+        //                         suma += tourists[LISTlodz[i].processid];
 
-                                if (suma <= lodziePojemnosc[wybieranaLodz])
-                                {
-                                    wycieczka.push_back(LISTlodz[i].processid); // dodanie turysty do wektora turystów wypływających, jeśli mieści się on w łodzi
-                                }
+        //                         if (suma <= lodziePojemnosc[wybieranaLodz])
+        //                         {
+        //                             wycieczka.push_back(LISTlodz[i].processid); // dodanie turysty do wektora turystów wypływających, jeśli mieści się on w łodzi
+        //                         }
 
-                                if (suma > lodziePojemnosc[wybieranaLodz])
-                                {
-                                    suma -= tourists[LISTlodz[i].processid];
-                                    continue;
-                                }
-                            }
+        //                         if (suma > lodziePojemnosc[wybieranaLodz])
+        //                         {
+        //                             suma -= tourists[LISTlodz[i].processid];
+        //                             continue;
+        //                         }
+        //                     }
 
-                            lamportPacket packetOut;
-                            packetOut.count = wycieczka.size(); // liczba turystów wypływających
-                            packetOut.lodz = wybieranaLodz;     // indeks łodzi wypływającej
-                            debug("3. wyplywam na wycieczke w lodzi: %d, wycieczka to turysci: %s", packetOut.lodz, stringVector(wycieczka).c_str());
-                            lamportSend(touristsId, FULLlodz, &lamportClock, packetOut); // wysłanie komunikatu FULLłódź do wszystkich turystów
-                            for (int i = 0; i < size; i++)                               // przesłanie tablicy z wypływającymi turystami bez zwiększenia zegaru Lamporta (jako część jednej wiadomości wraz z FULLłódź)
-                            {
-                                MPI_Send(wycieczka.data(), (int)wycieczka.size(), MPI_INT, i, DATA, MPI_COMM_WORLD);
-                            }
+        //                     lamportPacket packetOut;
+        //                     packetOut.count = wycieczka.size(); // liczba turystów wypływających
+        //                     packetOut.lodz = wybieranaLodz;     // indeks łodzi wypływającej
+        //                     debug("3. wyplywam na wycieczke w lodzi: %d, wycieczka to turysci: %s", packetOut.lodz, stringVector(wycieczka).c_str());
+        //                     lamportSend(touristsId, FULLlodz, &lamportClock, packetOut); // wysłanie komunikatu FULLłódź do wszystkich turystów
+        //                     for (int i = 0; i < size; i++)                               // przesłanie tablicy z wypływającymi turystami bez zwiększenia zegaru Lamporta (jako część jednej wiadomości wraz z FULLłódź)
+        //                     {
+        //                         MPI_Send(wycieczka.data(), (int)wycieczka.size(), MPI_INT, i, DATA, MPI_COMM_WORLD);
+        //                     }
 
-                            nadzorca = rank; // nadzorca to turysta, który wysyła komunikat FULLłódź i będzie wysyłać komunikat RELłódź
-                            changeState(Wycieczka);
-                        }
-                    }
-                    else
-                    {
-                        wybieranaLodz = (wybieranaLodz + 1) % lodzCount; // zmiana numeru wybieranej łodzi, jeśli obecnie wybierana nie jest w stanie oczekiwania
-                    }
-                }
-            }
-        }
+        //                     nadzorca = rank; // nadzorca to turysta, który wysyła komunikat FULLłódź i będzie wysyłać komunikat RELłódź
+        //                     changeState(Wycieczka);
+        //                 }
+        //             }
+        //             else
+        //             {
+        //                 wybieranaLodz = (wybieranaLodz + 1) % lodzCount; // zmiana numeru wybieranej łodzi, jeśli obecnie wybierana nie jest w stanie oczekiwania
+        //             }
+        //         }
+        //     }
+        // }
 
         if (stan == LodzTEST)
         {
-            // if (nadzorca == -1)
-            // {
 
             if (lodzieStan[wybieranaLodz] != 0) // jeżeli wybierana lodz jest dostepna
             {
@@ -145,7 +143,6 @@ void mainLoop()
             {
                 wybieranaLodz = (wybieranaLodz + 1) % lodzCount; // zmiana numeru wybieranej łodzi, jeśli obecnie wybierana nie jest w stanie oczekiwania
             }
-            // }
         }
 
         if (stan == Wycieczka)
